f95f0c976e814071c437b9a2463e974a811668f4
Otávio Wolff Buffon <100782160+taviowolff@users.noreply.github.com>
2022-04-20 15:26:36 -0300

    Create Aspas.py
    Aspas ou Quotation marks

---
+++ Aspas.py
@@ -0,0 +1,12 @@
+'''
+Nessa aula vou ensinar a usar Aspas ('') simples e ("") duplas
+'''
+print('olá mundo')  # sempre que começarmos uma aspas tanto simples quanto dupla devemos finalizar com a mesma
+
+print('ele estava sendo "engraçado".') # se você quiser utilizar aspas dentro de uma Str alterneas entre a simples e a dupla
+print("ele estava sendo 'engraçado'.")
+
+x = 'cuidado' 
+b = "be care"  # na definição de variáveis você pode usar qualquer uma dessas 
+
+print(x, b)  # ambas as frases aparecem tranquilamente sem problemas.
