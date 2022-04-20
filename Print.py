b427947ae0db6c3c2ed133b56cebaf80233d3d87
Otávio Wolff Buffon <100782160+taviowolff@users.noreply.github.com>
2022-04-20 10:39:01 -0300

    Create Print.py
    Here I am showing the print functionality

---
+++ Print.py
@@ -0,0 +1,5 @@
+print('Olá Mundo')  # aqui temos o comando (print) que faz basicamendo a impressão daquilo pedido em sua tela.
+print('Hello world')
+
+a = 'olá mundo'  
+print(a) # a frase 'olá mundo' é uma Str ela pode ser adicionada a uma variável e ser mostrada na tela como mostro nesse comando.
