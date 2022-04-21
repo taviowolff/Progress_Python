34425aa4f9f8b352ee3ab6f43e75c46515b3c1dd
Otávio Wolff Buffon <100782160+taviowolff@users.noreply.github.com>
2022-04-21 20:29:31 -0300

    Update 001Print.py

--- 001Print.py
+++ 001Print.py
@@ -1,6 +1,6 @@
 print('Olá Mundo')  # aqui temos o comando (print) que faz basicamendo a impressão daquilo pedido em sua tela.
 print('Hello world')  # here we have the command (print) that basically prints the request on your screen.
 
-a = 'olá mundo'  
+x = 'olá mundo'  
 print(a) # a frase 'olá mundo' é uma Str ela pode ser adicionada a uma variável e ser mostrada na tela como mostro nesse comando.
          # the phrase 'hello world' is a Str it can be added to a variable and be shown on the screen as I show in this command.
