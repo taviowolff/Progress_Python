bfcfb3850df1ca06d71f26fd834afad8257ff67a
Otávio Wolff Buffon <100782160+taviowolff@users.noreply.github.com>
2022-04-21 20:29:47 -0300

    Update 003Tipos_de_dados.py

--- 003Tipos_de_dados.py
+++ 003Tipos_de_dados.py
@@ -1,9 +1,9 @@
 '''
 tipos de dados 
-String - Str   ('Otávio', 'Wolff', 'Rio de Janeiro')
-Inteiro - Int   (1, 2, 3, 4, 5)
-Floating number - Float   (1.3 ; 0.5 ; 10.51)
-Booleanos - Bool   (True / False)
+String - Str  ('Otávio', 'Wolff', 'Rio de Janeiro')
+Inteiro - Int  (1, 2, 3, 4, 5)
+Floating number - Float  (1.3 ; 0.5 ; 10.51)
+Booleanos - Bool  (True / False)
 
 Nesta aula aprendemos a utilizaro comendo 'type', para descobrir qual tipo é o dado representado na linha.
 In this class you will learn how to use 'type', to find out what type is the data in the line.
