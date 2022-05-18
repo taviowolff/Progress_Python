d9d748f3fe4fd871d2d6cd1cb6a562ca537e75bd
Otávio Wolff Buffon <100782160+taviowolff@users.noreply.github.com>
2022-05-18 10:43:47 -0300

    delete

--- progress_python/001Print.py
+++
@@ -1,6 +0,0 @@
-print('Olá Mundo')  # aqui temos o comando (print) que faz basicamendo a impressão daquilo pedido em sua tela.
-print('Hello world')  # here we have the command (print) that basically prints the request on your screen.
-
-x = 'olá mundo'  
-print(a) # a frase 'olá mundo' é uma Str ela pode ser adicionada a uma variável e ser mostrada na tela como mostro nesse comando.
-         # the phrase 'hello world' is a Str it can be added to a variable and be shown on the screen as I show in this command.
