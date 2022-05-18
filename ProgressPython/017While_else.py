ea2a0ddd1183695b142f623e9313ccd87976a091
Otávio Wolff Buffon <100782160+taviowolff@users.noreply.github.com>
2022-05-18 10:44:40 -0300

    Create 017While_else.py

---
+++ ProgressPython/017While_else.py
@@ -0,0 +1,17 @@
+"""
+While / Else - Aula
+contadores
+acumuladores
+"""
+contador = 1
+acumulador = 1
+
+while contador <= 10:
+    print(contador, acumulador)
+
+    acumulador = acumulador + contador
+    contador += 1
+else:
+    print('sai daqui')
+
+
