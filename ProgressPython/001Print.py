750a09ca8886d7a5d6e0c83e7f48138f5ca76aa5
Otávio Wolff Buffon <100782160+taviowolff@users.noreply.github.com>
2022-07-02 14:25:08 -0300

    Update 001Print.py

--- ProgressPython/001Print.py
+++ ProgressPython/001Print.py
@@ -2,5 +2,5 @@ print('Olá Mundo')  # aqui temos o comando (print) que faz basicamendo a impres
 print('Hello world')  # here we have the command (print) that basically prints the request on your screen.
 
 x = 'olá mundo'  
-print(a) # a frase 'olá mundo' é uma Str ela pode ser adicionada a uma variável e ser mostrada na tela como mostro nesse comando.
+print(x) # a frase 'olá mundo' é uma Str ela pode ser adicionada a uma variável e ser mostrada na tela como mostro nesse comando.
          # the phrase 'hello world' is a Str it can be added to a variable and be shown on the screen as I show in this command.
