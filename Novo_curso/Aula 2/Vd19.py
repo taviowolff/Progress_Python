b33b8671bd2e9160cb7fbe560f7480613f5c386e
Otávio Wolff Buffon <100782160+taviowolff@users.noreply.github.com>
2023-08-28 18:59:06 -0300

    Aula 2 (2023/08/28)
    Aprendi essas classes e algumas funções
    String
    Int
    Float
    \
    r

--- Novo_curso/Aula 2/Vd19.py
+++ Novo_curso/Aula 2/Vd19.py
@@ -4,11 +4,19 @@ Tipo de ripagem = Dinâmica / Forte
 str -> string -> texo
 Strings são textos que estão dentro de aspas
 """
+print(1234)
 
 # Aspas simples
+print('otavio wolff')
+print('otavio "wolff"')
 
-# Aspas Duplas
+# Aspas duplas
+print("otavio wolff")
 
 # Escape
+print("otavio \"wolff") # esta \ barra invertida faz com que o leitor do python
+print("otavio \"wolff\"") # escape a leitura do próximo caractére
 
 # r
+print(r"\"Otávio\"") # esse r faz com que o caractere de escape apareça no terminal
+                     # serve para inspeções
