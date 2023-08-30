a7064600cda1506c82a35f5ce3af727fbb1d0628
Otávio Wolff Buffon <100782160+taviowolff@users.noreply.github.com>
2023-08-30 19:27:26 -0300

    Aula 3 (2023/08/30)
    Aprendi sobre o objeto primitivo boolean e sobre a imutabilidade de alguns objetos primitivos do python, juntamente como modifica-los (se possível) e concatena-los (se possível).

---
+++ Novo_curso/Aula 3/Vd21.py
@@ -0,0 +1,15 @@
+# Tipo de dado bool (boolean)
+# Ao questionar algo em um programa,
+# só existem duas respostas possíveis:
+# sim (True) ou não (False).
+# Existem vários operadores para "questionar"
+# Demtre eles o ==, que é um operador lógico que 
+# Questiona se um valor é igual a outro
+
+print(10 == 10) # Sim => True (Verdadeiro)
+print(10 == 11) # Não => False (Falso)
+
+# Todos esses type vão mostrar que o objeto dentro do () são boolean
+print(type(False))
+print(type(10 == 10))
+print(type(10 == 11))
---
+++ Novo_curso/Aula 3/Vd22.py
@@ -0,0 +1,10 @@
+# Conversão de tipos, coerção
+# type convertion, typecasting, coercion
+# é o ato de converter um tipo em outro
+# tipos imutáveis e primitivos:
+# str, int, float, bool
+
+print(int('1'), type(int('1'))) # aqui mostra o objeito str transformato para int e o tipo da classe do objeto
+print(type(float('1') + 1)) # mostra qual o tipo da classe do objeto
+print(float('1') + 1) # soma de float e o '1' de srt foi para float
+print(str(1) + ('b')) # Concatenação de str e o 1 foi transformado em str
