d189ca2a64b305dd4f423b35d704bc1564660d81
Otávio Wolff Buffon <100782160+taviowolff@users.noreply.github.com>
2023-09-12 10:55:18 -0300

    Update Vd33.py

--- Novo_curso/Aula 6/Vd33.py
+++ Novo_curso/Aula 6/Vd33.py
@@ -3,8 +3,17 @@
 # nome = input('Qual seu nome? ')
 # print(f'O seu nome é {nome}')
 
-numero_1 = int(input('Digite um número: '))
-numero_2 = int(input('Digite outro número: '))
 # recomendo nao fazer essa transformaçao de dado em cima da variável 
+# numero_1 = int(input('Digite um número: '))
+# numero_2 = int(input('Digite outro número: '))
 
-print(f'A soma dos números é {numero_1+numero_2}')
+# print(f'A soma dos números é {numero_1+numero_2}')
+
+# forma recomendada para facilitar futuras verificações
+numero1 = input('Escreva um número: ')
+numero2 = input('Escreva outro número: ')
+
+numero1 = int(numero1)
+numero2 = int(numero2)
+
+print(f'A soma dos números é {numero1+numero2}')
