d355a593b7670b72afdc7467a8fae71b42b3f419
Otávio Wolff Buffon <100782160+taviowolff@users.noreply.github.com>
2023-09-13 21:59:40 -0300

    Create Vd38.py

---
+++ Novo_curso/Aula 8/Vd38.py
@@ -0,0 +1,8 @@
+num1 = input('Escreva um valor: ')
+num2 = input('Escreva outro valor ')
+
+if num1 > num2:
+    print(f'{num1} é maior que {num2}')
+
+else:
+    print(f'{num2} é maior que {num1}')
