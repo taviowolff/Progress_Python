17d617482fa9cc24bd1a7b12f9f16b1d90acbd59
Otávio Wolff Buffon <100782160+taviowolff@users.noreply.github.com>
2023-09-16 12:53:53 -0300

    Update exAula9.py

--- Novo_curso/Aula 9/exAula9.py
+++ Novo_curso/Aula 9/exAula9.py
@@ -3,12 +3,21 @@
 # Aqui vou definir uma senha como se já estivessa salva em um suposto banco de dados
 senha_usario = '12345'
 
+# Verificador para saber se o usuário quer entrar
+entrada = input('[E]ntrar [S]sair: ')
+
 # Aqui estaremos estaremos definindo a senha que o usuário escreveu para futuras verificações com a senha_usario
 inserir_senha = input('Digite a senha: ')
 
-if inserir_senha == senha_usario:
+# Ocorre a verificação da senha do usuário com a senha inserida
+# Caso a senha também seja dado nenhuma valor
+# Se um desses dois retornar True as linhas de códigos serão lidas abaixo
+if inserir_senha != senha_usario or inserir_senha == '':
+    print('Senha incorreta')
+
+elif inserir_senha == senha_usario:
     print('Senha correta!')
     print('Você está logado')
 
 else:
-    print('Senha incorreta!')
+    print('Error!')
