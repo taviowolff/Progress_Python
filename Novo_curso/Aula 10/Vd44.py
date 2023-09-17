b09acf87630d80b99e483df8ff924aa9f7222ad9
Otávio Wolff Buffon <100782160+taviowolff@users.noreply.github.com>
2023-09-17 07:10:41 -0300

    update Aula 10

---
+++ Novo_curso/Aula 10/Vd44.py
@@ -0,0 +1,12 @@
+"""
+Interpolação básica de strings
+s - string
+d e i - int
+f - float
+x e X - hexadecimal (ABCDEF0123456789)
+"""
+
+nome = 'Otávio'
+preco = 1000.93412532
+varivel = '%s, o preço é de R$%f' % (nome, preco)
+print(varivel)
