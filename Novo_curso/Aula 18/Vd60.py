67ac60a23e4f92e833344f4c90c098d49582db8f
Otávio Wolff Buffon <100782160+taviowolff@users.noreply.github.com>
2023-09-26 15:35:07 -0300

    (2023/09/26)
    Matéria
    - while condições

---
+++ Novo_curso/Aula 18/Vd60.py
@@ -0,0 +1,25 @@
+"""
+Repetições
+while (enquanto)
+Executa uma ação enquanto uma condição for verdadeira
+Loop infinito -> Quando um código não tem fim
+"""
+# contador = 0
+
+# while contador <= 10:
+#     contador = contador + 1
+#     print(contador)
+
+# print('Acabou')
+
+verificador = True 
+
+while verificador:
+    if True:
+       
+        resposta = input('Você deseja sair do looping (s/n)? ')
+        
+        if resposta == 's':
+            verificador = False
+
+print('você não saiu do loop')
