2fb083bcd15a80076d701224c702e3376f082d4b
Otávio Wolff Buffon <100782160+taviowolff@users.noreply.github.com>
2023-09-28 19:53:12 -0300

    Update Atv64.py

--- Novo_curso/Aula 19/Atv64.py
+++ Novo_curso/Aula 19/Atv64.py
@@ -1,21 +1,37 @@
 """
 faça um str iterável com while
 """
-nome = 'Otávio Wolff'
+# nome = 'Otávio Wolff'
+
+# nova_str = ''
+
+# x = 0
+
+# while x < len(nome):
+
+#     lpl = nome[x]
+
+#     nova_str += (f'-{lpl}')
+    
+#     x += 1
+
+#     print(nova_str)
+    
+
+# print(nova_str)
+
+nome = 'Otávio'
 
 nova_str = ''
 
 x = 0
 
-while x < len(nome):
+while x <= len(nome):
 
-    lpl = nome[x]
+    letras = nome[x]
 
-    nova_str += (f'-{lpl}')
-    
+    nova_str += letras
+
     x += 1
 
     print(nova_str)
-    
-
-# print(nova_str)
