7d97acd7c3c37eb7f59629629358e6a8873fe229
Otávio Wolff Buffon <100782160+taviowolff@users.noreply.github.com>
2023-10-12 21:45:15 -0300

    Create Vd72.py

---
+++ Novo_curso/Aula 23/Vd72.py
@@ -0,0 +1,26 @@
+"""
+For + Range
+range -> range(start, stop, step)
+"""
+# numeros = range(0, 100, 8)
+
+# for numero in numeros:
+#     print(numero)
+
+"""
+Range não depende de for e for não depende de range
+O range() é uma ferramenta útil para gerar sequências numéricas e é comumente usado em loops 
+for quando você precisa iterar um número específico de vezes ou quando deseja criar um loop 
+que trabalhe com um conjunto específico de valores.
+"""
+
+# nome = 'Otávio Wolff Buffon'
+
+# for letras in nome:
+#     print(letras)
+
+
+numeros = range(0,21,2)
+
+for numero in numeros:
+    print(numero)
