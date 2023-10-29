c82720fe71e2754aecea6e68f007b20e511b4c77
Otávio Wolff Buffon <100782160+taviowolff@users.noreply.github.com>
2023-10-29 12:11:56 -0300

    Create Atv5.py

---
+++ Novo_curso/Testes/GG/Unidade 1/Atv5.py
@@ -0,0 +1,9 @@
+'''
+Digite um número e mostre seu antecessor e sucessor
+'''
+
+numero = int(input('Defina um número: '))
+
+# print(f'O antecessor de {numero} é {numero - 1} e o sucessor é {numero + 1}')
+
+range(numero)
