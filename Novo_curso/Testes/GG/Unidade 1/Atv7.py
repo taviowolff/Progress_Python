bb23dad4bae3a32cc735a3181142af97df0ffc6c
Otávio Wolff Buffon <100782160+taviowolff@users.noreply.github.com>
2023-10-29 12:12:00 -0300

    Create Atv7.py

---
+++ Novo_curso/Testes/GG/Unidade 1/Atv7.py
@@ -0,0 +1,9 @@
+''' 
+Média aritmética
+'''
+
+nota1 = float(input('nota na Matéria 1 do aluno: '))
+
+nota2 = float(input('nota na Matéria 2 do aluno: '))
+
+print(f'A média das notas do aluno foi {(nota1+nota2)/2}')
