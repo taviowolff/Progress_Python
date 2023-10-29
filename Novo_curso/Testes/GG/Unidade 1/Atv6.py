a9bca2660af8ca3a71f7adffc6583c60df0470dc
Otávio Wolff Buffon <100782160+taviowolff@users.noreply.github.com>
2023-10-29 12:11:58 -0300

    Create Atv6.py

---
+++ Novo_curso/Testes/GG/Unidade 1/Atv6.py
@@ -0,0 +1,12 @@
+'''
+Digitar um número e retornar o dobro, o triplo e a raiz quadrada
+'''
+
+numero = float(input('Digite um número: '))
+
+print(f'O dobro de {numero} é {numero * 2}')
+
+print(f'O triplo de {numero} é {numero * 3:.2f}')
+
+
+print(f'A raiz quadrada de {numero} é {numero ** (1/2):.2f}')
