05d061bc704597ad93120d30d89f2d7160e91800
Otávio Wolff Buffon <100782160+taviowolff@users.noreply.github.com>
2023-10-31 17:28:32 -0300

    Update Atv8.py

--- Novo_curso/Testes/GG/Unidade 1/Atv8.py
+++ Novo_curso/Testes/GG/Unidade 1/Atv8.py
@@ -1,5 +1,12 @@
 '''
-Escreva um programa que leia um valor em metros e o exiba convertido em centimetros e milímetros
+Escreva um programa que leia um valor em metros e o exiba convertido 
+em centimetros e milímetros
 '''
 
-n = float(input('Sua algtura em metros'))
+n = float(input('Sua algtura em metros'))\
+
+cm = n*100
+
+mm = n*1000
+
+print(f'Sua altura {n}m em cm é {cm} e em mm é {mm} ')
