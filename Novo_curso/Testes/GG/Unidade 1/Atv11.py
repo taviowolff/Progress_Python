effda387aacd15cf9054266aeee5a1db7ffa8039
Otávio Wolff Buffon <100782160+taviowolff@users.noreply.github.com>
2023-10-31 17:28:29 -0300

    Update Atv11.py

--- Novo_curso/Testes/GG/Unidade 1/Atv11.py
+++ Novo_curso/Testes/GG/Unidade 1/Atv11.py
@@ -3,3 +3,13 @@ Faça um programa que leia a largura e a altura de uma parede
 em metros, calcule a sua área e a quantidade de tinta necessária
 para pintá-la, sabendo que cada litro de tinta, pinta uma área de 2m²
 '''
+
+larg = float(input('Qual é largura da parede? '))
+
+alt = float(input('Qual é a altura da parede? '))
+
+m2 = larg * alt
+
+tinta = m2 / 2
+
+print(f'Você vai precisar de {tinta} litros de tinta para pintar sua parede')
