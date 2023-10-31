d28250971cff75d1e82675c6dbd144c389c159f8
Otávio Wolff Buffon <100782160+taviowolff@users.noreply.github.com>
2023-10-31 17:28:26 -0300

    Update Atv10.py

--- Novo_curso/Testes/GG/Unidade 1/Atv10.py
+++ Novo_curso/Testes/GG/Unidade 1/Atv10.py
@@ -1,3 +1,13 @@
 '''
-Crie um programa que leia quanto dinheiro uma pessoa tem na carteira a mo
+Crie um programa que leia quanto dinheiro uma pessoa tem na 
+carteira e mostre quantos dólares ela pode comprar
 '''
+
+salario = float(input('Quanto dinheiro você tem em conta?: '))
+
+dolar = 5.04
+
+print(f'Você pode comprar {salario / dolar:.2f} Dólares')
+
+
+
