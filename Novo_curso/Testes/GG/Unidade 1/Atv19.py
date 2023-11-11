db517e3f8cda004e7cf85eeca6eeb6278fe523c4
Otávio Wolff Buffon <100782160+taviowolff@users.noreply.github.com>
2023-11-10 21:13:14 -0300

    Update Atv19.py

--- Novo_curso/Testes/GG/Unidade 1/Atv19.py
+++ Novo_curso/Testes/GG/Unidade 1/Atv19.py
@@ -3,3 +3,11 @@ Um professor quer sortear um dos seus quatro alunos para apagar
 o quadro. Faça um programa que ajude ele, lendo o nome deles e
 escrevendo o nome do escolhido.
 '''
+import random
+print('Professor vai escolher um dos 4 alunos da lista')
+
+lista_alunos = ['Jorge', 'Otávio', 'Danilo', 'Artur']
+
+aluno_escolhido = random.choice(lista_alunos)
+
+print(f'O professor escolheu {aluno_escolhido}')
