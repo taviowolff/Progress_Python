0a3c9d64dd7eda0ddabd895a0c70c02eee8a87a3
Otávio Wolff Buffon <100782160+taviowolff@users.noreply.github.com>
2023-11-12 21:55:40 -0300

    Update VdAtv98.py

--- Novo_curso/Aulas/Aula 33/VdAtv98.py
+++ Novo_curso/Aulas/Aula 33/VdAtv98.py
@@ -23,26 +23,49 @@ contrário disso:
 
 O primeiro dígito do CPF é 7
 """
-import math
-import os
-import time
+# Esse código consegue verificar o primeiro digito do cpf de uma maneira mais complexa
 
-cpf = input('Escreva seu cpf: ')
-lista_num = list(cpf)
-contador = 10
-nova_lista = []
+# cpf = input('Escreva seu cpf: ')
+# lista_num = list(cpf)
+# contador = 10
+# nova_lista = []
 
-while contador >= 3:
-    for num in lista_num:
-        num = int(num)
-        mult = contador * num
-        nova_lista.append(mult)
-        contador -= 1
+# while contador >= 3:
+#     for num in lista_num:
+#         num = int(num)
+#         mult = contador * num
+#         nova_lista.append(mult)
+#         contador -= 1
 
-nova_lista.pop(-1)
-nova_lista.pop(-1)
-soma_da_lista = 0
+# # nova_lista.pop(-1)
+# # nova_lista.pop(-1)
+# soma_da_lista = 0
 
-for elemento in nova_lista:
-    soma_da_lista += elemento
-print(soma_da_lista)
+# for elemento in nova_lista:
+#     soma_da_lista += elemento
+# print(soma_da_lista)
+
+# lista_mult = soma_da_lista * 10
+
+# lista_rest = lista_mult % 11 
+
+# if lista_rest > 9:
+#     resultado = 0
+
+# else:
+#     resultado = lista_rest
+# print(f'O primeiro digito do CPF é {resultado}')
+
+# Esse que eu vou escrever daqui é a partir do que eu vi do professor
+
+cpf = "15494868028"
+nove_digitos = cpf[:9]
+resultado_digito1 = 0
+contador_regressivo_1 = 10
+
+for digito_1 in nove_digitos:
+    resultado_digito1 += int(digito_1) * contador_regressivo_1
+    contador_regressivo_1 -= 1
+digito_1 = ((resultado_digito1 * 10) % 11)
+digito_1 = digito_1 if digito_1 <= 9 else 0
+print(digito_1)
