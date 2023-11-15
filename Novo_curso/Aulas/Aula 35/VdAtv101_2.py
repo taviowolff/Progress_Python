b253236e1a1711631fe6c5cfe700945a1f7d4673
Otávio Wolff Buffon <100782160+taviowolff@users.noreply.github.com>
2023-11-14 21:28:16 -0300

    Update VdAtv101_2.py

--- Novo_curso/Aulas/Aula 35/VdAtv101_2.py
+++ Novo_curso/Aulas/Aula 35/VdAtv101_2.py
@@ -23,3 +23,6 @@ digito_2 = ((resultado_digito2 * 10) % 11)
 digito_2 = digito_2 if digito_2 <= 9 else 0
 
 print(f'{nove_digitos}{digito_1}{digito_2}')
+print(type(digito_1))
+print(type(digito_2)) 
+print(type(nove_digitos))
