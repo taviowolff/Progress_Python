736389def10edd3a4eedeb875526dd212fa6f4fc
Otávio Wolff Buffon <100782160+taviowolff@users.noreply.github.com>
2023-11-16 11:07:59 -0300

    Update VdAtv101_2.py

--- Novo_curso/Aulas/Aula 35/VdAtv101_2.py
+++ Novo_curso/Aulas/Aula 35/VdAtv101_2.py
@@ -24,6 +24,3 @@ for cpf in range(100):
     digito_2 = digito_2 if digito_2 <= 9 else 0
 
     print(f'{nove_digitos}{digito_1}{digito_2}')
-    # print(type(digito_1))
-    # print(type(digito_2)) 
-    # print(type(nove_digitos))
