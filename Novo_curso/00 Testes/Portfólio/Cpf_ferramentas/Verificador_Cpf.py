131173e9aa1a2271e291506318c6ab2819b073b5
Otávio Wolff Buffon <100782160+taviowolff@users.noreply.github.com>
2023-11-16 11:07:51 -0300

    Create Verificador_Cpf.py

---
+++ Novo_curso/00 Testes/Portfólio/Cpf_ferramentas/Verificador_Cpf.py
@@ -0,0 +1,38 @@
+import random
+import re
+import sys
+
+cpf_verificar = input('Seu CPF:')
+
+cpf_enviado_usuario = re.sub(
+    r'[^0-9]',
+    '',
+    cpf_verificar
+)
+
+for cpf in range(2):
+    
+
+# for i in range(9):
+#     nove_digitos += str(random.randint(0,9))
+
+contador_regressivo_1 = 10
+
+resultado_digito1 = 0
+for digito_1 in cpf_enviado_usuario:
+    resultado_digito1 += int(digito_1) * contador_regressivo_1
+    contador_regressivo_1 -= 1
+digito_1 = ((resultado_digito1 * 10) % 11)
+digito_1 = digito_1 if digito_1 <= 9 else 0
+
+dez_digitos = cpf_enviado_usuario + str(digito_1)
+contador_regressivo_2 = 11
+
+resultado_digito2 = 0
+for digito_2 in dez_digitos:
+    resultado_digito2 += int(digito_2) * contador_regressivo_2
+    contador_regressivo_2 -= 1
+digito_2 = ((resultado_digito2 * 10) % 11)
+digito_2 = digito_2 if digito_2 <= 9 else 0
+
+print(f'{cpf_enviado_usuario}{digito_1}{digito_2}')
