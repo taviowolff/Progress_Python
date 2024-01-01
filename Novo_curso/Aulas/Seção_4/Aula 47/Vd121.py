61f022d565309cbdae33bfc5c1e1c831a18fbcdc
Otávio Wolff Buffon <100782160+taviowolff@users.noreply.github.com>
2024-01-01 18:33:41 -0300

    Update Vd121.py

--- Novo_curso/Aulas/Seção_4/Aula 47/Vd121.py
+++ Novo_curso/Aulas/Seção_4/Aula 47/Vd121.py
@@ -4,11 +4,7 @@
 # values - iterável com os valores
 # items - iterável com chaves e valores
 # setdefault - adiciona valor se a chave não existe
-# copy - retorna uma cópia rasa (shallow copy)
 # get - obtém uma chave
-# pop - Apaga um item com a chave especificada (del)
-# popitem - Apaga o último item adicionado
-# update - Atualiza um dicionário com outro
 
 pessoa = {
     'nome' : 'Otávio',
