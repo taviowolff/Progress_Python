fe434fad2d907958c9d319cb864cb1c9d5f7e73a
Otávio Wolff Buffon <100782160+taviowolff@users.noreply.github.com>
2024-01-01 18:33:44 -0300

    Create Vd123.py

---
+++ Novo_curso/Aulas/Seção_4/Aula 48/Vd123.py
@@ -0,0 +1,10 @@
+# pop - Apaga um item com a chave especificada (del)
+# popitem - Apaga o último item adicionado
+# update - Atualiza um dicionário com outro
+
+pessoa = {
+    'nome' : 'Otávio',
+    'idade' : '25',
+    'sobrenome' : 'Buffon'
+}
+
