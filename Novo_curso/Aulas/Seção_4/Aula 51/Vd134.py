a09a0ecc59dc2720f25935cf19a017362a9e9226
Otávio Wolff Buffon <100782160+taviowolff@users.noreply.github.com>
2024-01-12 16:09:16 -0300

    Update Vd134.py

---
+++ Novo_curso/Aulas/Seção_4/Aula 51/Vd134.py
@@ -0,0 +1,34 @@
+def executa(funcao, *args):
+    return funcao(*args)
+
+
+def soma(x, y):
+    return x + y
+
+
+def cria_multiplicador(multiplicador):
+    def multiplica(numero):
+        return numero * multiplicador
+    return multiplica
+
+# as três meneiras de executar a soma
+# print(executa(lambda x, y: x + y, 2, 3), 
+#       executa(soma, 2, 3),
+#       soma(2,3)
+#     )
+
+# duplica = cria_multiplicador(2)
+duplica = executa(lambda m: lambda n: n * m, 2)
+print(duplica(2))
+
+# lamda geralmente é para coisas rápidas e fáceis como uma soma simples 
+# nesse exemplo foi usado em uma função que retorna uma função o que causa uma confusão 
+
+# def multiplica(x,y):
+#     return x * y
+    
+print(executa(lambda x, y: x * y, 3, 2))
+
+soma = (lambda x, y: x * y)
+
+print(soma(2,3))
