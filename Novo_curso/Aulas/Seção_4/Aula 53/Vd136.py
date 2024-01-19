a3936aac1d8b8da047591567f547e1e1595e65d4
Otávio Wolff Buffon <100782160+taviowolff@users.noreply.github.com>
2024-01-19 08:33:44 -0300

    Create Vd136.py

---
+++ Novo_curso/Aulas/Seção_4/Aula 53/Vd136.py
@@ -0,0 +1,14 @@
+"""
+list comprehension
+é basicamente um for dentro de uma lista
+"""
+lista1 = []
+for n in range(10):
+    lista1.append(n)
+    
+print(lista1)
+
+
+lista = [numero for numero in range(10)]
+
+print(lista)
