668b3704f21bc470cf701587c20122884cb648db
Otávio Wolff Buffon <100782160+taviowolff@users.noreply.github.com>
2024-01-19 12:53:49 -0300

    Create Vd138.py

---
+++ Novo_curso/Aulas/Seção_4/Aula 54/Vd138.py
@@ -0,0 +1,23 @@
+# Filter list comprehension
+
+produtos = [
+    {'nome': 'p1', 'preco': 20, },
+    {'nome': 'p2', 'preco': 10, },
+    {'nome': 'p3', 'preco': 30, },
+]
+
+novos_produtos = [ 
+    {**produto, 'preco': produto['preco'] *  1.05}
+    if produto['preco'] > 20 else {**produto}
+    for produto in produtos
+]
+
+# print(*novos_produtos, sep='\n')
+
+novos_produtos = [ 
+    {**produto, 'preco': produto['preco'] * 1.05}
+    if produto['preco'] > 20 else {**produto}
+    for produto in produtos
+    if (produto['preco'] >= 20 and produto['preco']* 1.05) > 10
+]
+print(novos_produtos)
