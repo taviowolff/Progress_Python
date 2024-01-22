b4ff02b4009c1e8aadd02aef139fc2372f6c18aa
Otávio Wolff Buffon <100782160+taviowolff@users.noreply.github.com>
2024-01-22 13:06:25 -0300

    Create atv9.py

---
+++ Novo_curso/00 Testes/ListaDeExercicios/EstruturaSequencial/atv9.py
@@ -0,0 +1,5 @@
+temp_Fahrenheit = float(input('Qual a temperatura em graus Fahrenhet? '))
+temp_Celsius = 5 * ((temp_Fahrenheit - 32)/9)
+
+print(f'A temperatura em Graus Celsius é de {temp_Celsius}')
+
