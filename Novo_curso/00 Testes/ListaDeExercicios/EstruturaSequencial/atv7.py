609a474ee4fa00ca7f3b3e1dd277fe0cea1960f5
Otávio Wolff Buffon <100782160+taviowolff@users.noreply.github.com>
2024-01-22 13:06:21 -0300

    Create atv7.py

---
+++ Novo_curso/00 Testes/ListaDeExercicios/EstruturaSequencial/atv7.py
@@ -0,0 +1,4 @@
+lado_1 = float(input('Dê a medida de um lado do qudrado: '))
+lado_2 = float(input('Dê a outra medida: '))
+area_quadrado = lado_1*lado_2
+print(f'A área do seu quadrado é de {area_quadrado} e seu dobro é {area_quadrado*2}')
