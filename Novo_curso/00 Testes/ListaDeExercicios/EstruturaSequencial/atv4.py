fb889e27bae2514c4f6371cb433eb15c364d5c55
Otávio Wolff Buffon <100782160+taviowolff@users.noreply.github.com>
2024-01-22 13:06:12 -0300

    Create atv4.py

---
+++ Novo_curso/00 Testes/ListaDeExercicios/EstruturaSequencial/atv4.py
@@ -0,0 +1,8 @@
+bimestre_1 = float(input('Nota do primeiro semestre: '))
+bimestre_2 = float(input('Nota do segundo semestre: '))
+bimestre_3 = float(input('Nota do terceiro semestre: '))
+bimestre_4 = float(input('Nota do quarto semestre: '))
+
+media_notas = (bimestre_1+bimestre_2+bimestre_3+bimestre_4) / 4
+
+print(f'A média das notas desse aluno será: {media_notas}')
