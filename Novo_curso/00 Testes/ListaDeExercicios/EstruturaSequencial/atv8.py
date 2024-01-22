7deeefa59a3403dede94add57d9bb0a0121b6abe
Otávio Wolff Buffon <100782160+taviowolff@users.noreply.github.com>
2024-01-22 13:06:23 -0300

    Create atv8.py

---
+++ Novo_curso/00 Testes/ListaDeExercicios/EstruturaSequencial/atv8.py
@@ -0,0 +1,5 @@
+salario_hora = float(input('Quanto você ganha por hora? '))
+horas_trabalhos = float(input('Quano tempo trabalhou esse mês? '))
+ganho_mensal = salario_hora * horas_trabalhos
+
+print(f'Esse mês você ganhou {ganho_mensal}')
