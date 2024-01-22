bbf036d467d02a4e1abf636d70e4809c7405a693
Otávio Wolff Buffon <100782160+taviowolff@users.noreply.github.com>
2024-01-22 13:06:18 -0300

    Create atv6.py

---
+++ Novo_curso/00 Testes/ListaDeExercicios/EstruturaSequencial/atv6.py
@@ -0,0 +1,4 @@
+raio_circulo = float(input("Dê o raio de uma circuferência: "))
+print('Levando em consideração pi = 3,14')
+area_circulo = 3.14*(raio_circulo**2)
+print(f"A área do círculo é {area_circulo}")
