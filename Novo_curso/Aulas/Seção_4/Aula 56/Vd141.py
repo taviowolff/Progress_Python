995ed61f86a5f38ce7b5b73458f9b70d60dbb194
Otávio Wolff Buffon <100782160+taviowolff@users.noreply.github.com>
2024-01-29 18:11:44 -0300

    Update Vd141.py

--- Novo_curso/Aulas/Seção_4/Aula 56/Vd141.py
+++ Novo_curso/Aulas/Seção_4/Aula 56/Vd141.py
@@ -1,15 +1,34 @@
-dicionario = {
-    'produto': 'Caneta-preta',
-    'valor': 2.50,
-    'marca': 'BIC'
-}
+# dicionario = {
+#     'produto': 'Caneta-preta',
+#     'valor': 2.50,
+#     'marca': 'BIC'
+# }
 
-dc = {
-    chave.upper(): valor
-    if isinstance(chave, str) else chave
-    for chave, valor 
-    in dicionario.items()
-    # if chave == 'valor'   
-}
+# dc = {
+#     chave.upper(): valor
+#     if isinstance(chave, str) else chave
+#     for chave, valor 
+#     in dicionario.items()
+#     # if chave == 'valor'   
+# }
 
-print(dc)
+# lista = [
+#     ('a', 'valor a'),
+#     ('b', 'valor b'),
+#     ('c', 'valor c')
+# ]
+
+# dc1 = {
+#     chave: valor
+#     for chave, valor in lista
+# }
+
+# print(dc1)
+
+
+lista = [0,1,2,3,4,5,6,7,8,9]
+
+set = set(lista)
+
+
+print(set)
