9ec87cbd9bf63d8b272c0d64da5f0c5124ad8699
Otávio Wolff Buffon <100782160+taviowolff@users.noreply.github.com>
2024-02-03 08:04:00 -0300

    Create atv13.py

---
+++ Novo_curso/00 Testes/ListaDeExercicios/EstruturaSequencial/atv13.py
@@ -0,0 +1,5 @@
+altura = 1.71
+peso_ideal_fem = (62.1*altura)-44.7
+peso_ideal_masc = (72.7*altura)-58
+
+print(f'Se você for mulher seu peso idela é {peso_ideal_fem} caso seja homem seu peso ideal é {peso_ideal_masc}')
