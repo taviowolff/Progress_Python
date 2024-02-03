ca58d462eb9094bd55ee7015815abaee7b15392f
Otávio Wolff Buffon <100782160+taviowolff@users.noreply.github.com>
2024-02-03 08:03:55 -0300

    Create atv11.py

---
+++ Novo_curso/00 Testes/ListaDeExercicios/EstruturaSequencial/atv11.py
@@ -0,0 +1,10 @@
+num_1 = int(input('Escreva um número inteiro: '))
+num_2 = int(input('Escreva outro número inteiro: '))
+num_real = float(input('Escreva um número real: '))
+
+a = (num_1 * 2)*(num_2/2)
+print(a)
+b = (num_1*3) + num_real
+print(b)
+c = num_real**3
+print(c)
