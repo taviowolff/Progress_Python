0ae52950e1a36b84a9d60cf47454a59940473367
Otávio Wolff Buffon <100782160+taviowolff@users.noreply.github.com>
2024-02-27 20:41:34 -0300

    Create Vd153.py

---
+++ Novo_curso/Aulas/Seção_4/Aula 66/Vd153.py
@@ -0,0 +1,18 @@
+# Módulos padrão do Python (import, fromm as e *)
+# https://docs.python.org/3/py-modindex.html
+# inteiro - import nome_modulo
+# Vantagens: você tem o namescape do módulo
+# Desvatagens: nomes grandes
+
+# partes - from nome_modulo import objeto1, objeto2
+# Vantagens: nomes pequenos
+# Desvantagens: Sem o namespace do módulo
+
+# alias 1 - import nome_modulo as apelido
+# alias 2 - from nome_modulo import objeto as apelido
+# Vantagens: você pode reversar nomes para seu código
+# Desvantagens: pode ficar fora do padrão da linguagem
+
+# má prática - from nome_modulo import *
+# Vantagens: importa tudo de um módulo
+# Desvantagens: importa tudo de um módulo
