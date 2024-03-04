8bc3b1002f8c1d26eb0b0b7fe9e4f8bb4e83daf3
Otávio Wolff Buffon <100782160+taviowolff@users.noreply.github.com>
2024-03-04 18:48:50 -0300

    Update Lista_de_tarefas.py

--- Novo_curso/Progresso/Portfólio/Python/Lista_de_tarefas/Lista_de_tarefas.py
+++ Novo_curso/Progresso/Portfólio/Python/Lista_de_tarefas/Lista_de_tarefas.py
@@ -9,37 +9,51 @@ while True:
     criar_lista = input('Deseja criar uma lista? s/n: ')
 
     while criar_lista == 's':
-            escolha = input('Deseja [1]adicionar, [2]mostrar, [3]deletar, [4]finalizar da lista? ')
-           
-            if escolha == '1':
-                os.system('cls')
-                item = input('Adicionar tarefa: ')
-                tarefas.append(item)
-                print('Item adicionado com sucesso!')
-                sleep(2)
+        escolha = input('Deseja [1]adicionar, [2]mostrar, [3]deletar, [4]finalizar da lista? ')
+        
+        if escolha == '1':
+            os.system('cls')
+            item = input('Adicionar tarefa: ')
+            tarefas.append(item)
+            print('Item adicionado com sucesso!')
+            sleep(2)
 
-            elif escolha == '2':
-                for tarefa in tarefas:
-                    print(tarefa)
-                sleep(5)
-                os.system('cls')
+        elif escolha == '2':
+            os.system('cls')
+            for tarefa in tarefas:
+                print(tarefa)
+            sleep(5)
+            os.system('cls')
 
-            elif escolha == '3':
-                for indice, tarefa in enumerate(tarefas):
-                    print(f'{indice}:{tarefa}')    
-                deletar = int(input('Escolha o indece para deletar: '))
-                try:
-                    del tarefas[deletar]
-                except IndexError:
-                    print('não tem esse indice na lista')
-                sleep(2)
-                os.system('cls')
+        elif escolha == '3':
+            for indice, tarefa in enumerate(tarefas):
+                print(f'{indice}:{tarefa}')    
+            deletar = int(input('Escolha o índice para deletar: '))
+            try:
+                del tarefas[deletar]
+            except IndexError:
+                print('Não tem esse índice na lista')
+            except ValueError:
+                print('Escolha um número como índice')
+            sleep(2)
+            os.system('cls')
 
-            elif escolha == '4':
-                for tarefa in tarefas:
-                    print(tarefa)
-                print('Sua lista final')    
-                break
+        elif escolha == '4':
+            for tarefa in tarefas:
+                print(tarefa)
+            print('Sua lista final')    
+            break
 
-            else:
-                print('Escolheu número errado')
+        else:
+            print('Escolheu número errado')
+    else:
+        criar_lista = input('Deseja retornar a lista? s/n: ')
+      
+        if escolha == 's':
+            continue   
+        else:
+            print('Você não criou a lista')
+            sleep(2)
+            os.system('cls')
+            break
+        
