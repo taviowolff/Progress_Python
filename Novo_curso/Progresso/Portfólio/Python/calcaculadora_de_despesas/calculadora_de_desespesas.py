699b38a6afab1e600b72d1c3f3e1c2f9d64dd6cc
Otávio Wolff Buffon <100782160+taviowolff@users.noreply.github.com>
2024-03-06 19:39:06 -0300

    Create calculadora_de_desespesas.py

---
+++ Novo_curso/Progresso/Portfólio/Python/calcaculadora_de_despesas/calculadora_de_desespesas.py
@@ -0,0 +1,14 @@
+def calcular_despesas(despesas):
+    total = sum(despesas)
+    return total
+
+despesas = []
+
+while True:
+    valor = float(input('Digite o valor da despesa(0 para sair): '))
+    if valor == 0:
+        break
+    despesas.append(valor)
+
+total_despesas = calcular_despesas(despesas)
+print(f'O valor total das despesas foi de R${total_despesas}')
