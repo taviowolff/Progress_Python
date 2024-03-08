20ee4175e851a381401eefa97af84d9b01c74f1c
Otávio Wolff Buffon <100782160+taviowolff@users.noreply.github.com>
2024-03-08 16:44:01 -0300

    Update Vd153.py

--- Novo_curso/Aulas/Seção_4/Aula 66/Vd153.py
+++ Novo_curso/Aulas/Seção_4/Aula 66/Vd153.py
@@ -4,15 +4,51 @@
 # Vantagens: você tem o namescape do módulo
 # Desvatagens: nomes grandes
 
+# import sys
+
+# platform = 'A minha plataforma'
+# print(sys.platform, 'Planataforma do sys')
+# print(platform)
+
+
 # partes - from nome_modulo import objeto1, objeto2
 # Vantagens: nomes pequenos
 # Desvantagens: Sem o namespace do módulo
 
+# from sys import exit, platform
+
+# print(platform)
+
+# platform = 'A minha' # subreescrenveu a função do módulo com a variável 
+
+# print(platform)
+
 # alias 1 - import nome_modulo as apelido
+# import sys as s
+# sys = 'alguma coisa'
+# print(s.platform)
+# print(sys)
+
 # alias 2 - from nome_modulo import objeto as apelido
+# from sys import platform as pf, exit as ex
+
+# print(pf)
+
 # Vantagens: você pode reversar nomes para seu código
 # Desvantagens: pode ficar fora do padrão da linguagem
+
 
 # má prática - from nome_modulo import *
 # Vantagens: importa tudo de um módulo
 # Desvantagens: importa tudo de um módulo
+
+from sys import *
+import os
+from time import sleep
+
+
+print(platform)
+sleep(2)
+os.system('cls')
+
+exit()
