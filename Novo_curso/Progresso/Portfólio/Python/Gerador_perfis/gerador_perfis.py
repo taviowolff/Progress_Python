2deefe807ae2358b1fc00596f576bd74e2be9c51
Otávio Wolff Buffon <100782160+taviowolff@users.noreply.github.com>
2024-03-08 16:43:50 -0300

    Create gerador_perfis.py

---
+++ Novo_curso/Progresso/Portfólio/Python/Gerador_perfis/gerador_perfis.py
@@ -0,0 +1,5 @@
+import random
+
+primeiro_nome = ["Acauã","Anaíra","Beto","Camila","Davi","Elisa","Felipe","Gabriela","Henrique","Isabela","João","Júlia","Kauê","Laura","Matheus","Mariana","Noah","Olivia","Pedro","Rafaela",]
+
+segundo_nome = ["Silva", "Souza", "Costa", "Santos", "Oliveira", "Pereira", "Barbosa", "Almeida", "Martins", "Fernandes", "Gomes", "Rodrigues", "Dias", "Carvalho", "Araujo", "Monteiro", "Ribeiro", "Gonçalves", "Pires", "Mendes"]
