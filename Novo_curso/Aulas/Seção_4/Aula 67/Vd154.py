70a98a9959fe3ac2d69b38d4c831e46439a650f7
Otávio Wolff Buffon <100782160+taviowolff@users.noreply.github.com>
2024-03-11 11:44:57 -0300

    Create Vd154.py

---
+++ Novo_curso/Aulas/Seção_4/Aula 67/Vd154.py
@@ -0,0 +1,13 @@
+# entendendo os seus próprios módulos Python
+# O primeiro módulo executado chama-se __main__ 
+# Você pode importar outro módulo inteiro ou parte do módulo
+# O python conhece a pasta onde o __main__ está e as pastas
+# abaixo dele
+# Ele não reconhece pastas e módulos acima do __main__ por
+# padrão
+# O python conhece todos os módulos e pacotes presentes
+# nos caminhos de sys.path
+
+# print('Essa é da aula 154')
+
+variavel_aula154 = 'importação feita'
