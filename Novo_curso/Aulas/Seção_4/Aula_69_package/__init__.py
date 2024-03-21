9e5bf9cffc2a7d9618f1c997704f72ae2440d6e5
Otávio Wolff Buffon <100782160+taviowolff@users.noreply.github.com>
2024-03-21 09:55:03 -0300

    Create __init__.py

---
+++ Novo_curso/Aulas/Seção_4/Aula_69_package/__init__.py
@@ -0,0 +1,9 @@
+print(
+    'Você importou', __name__
+)
+
+# qualquer coisa feita aqui vai ser executada no package que está importado na aula69.py
+# pode ser usado para "enganar" o python
+
+def dobra(x):
+    return x*2
