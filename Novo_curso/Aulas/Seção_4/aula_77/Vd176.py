ec0ef33d2c542e1862a1430a434f7cbfbc253df2
Otávio Wolff Buffon <100782160+taviowolff@users.noreply.github.com>
2024-05-11 23:13:09 -0300

    Update Vd176.py

--- Novo_curso/Aulas/Seção_4/aula_77/Vd176.py
+++ Novo_curso/Aulas/Seção_4/aula_77/Vd176.py
@@ -1,3 +1,15 @@
 from functools import partial
 from types import GeneratorType
 
+def dobrar(numero):
+    return numero * 2
+
+
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# quadrados = list(map(lambda n: n ** 2, numeros))
+
+# print(quadrados)
+
+
+dobrados = list(map(dobrar, numeros))
+print(dobrados)
