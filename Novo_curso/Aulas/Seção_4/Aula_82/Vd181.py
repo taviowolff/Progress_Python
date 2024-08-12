a0734ceff01f94dcbf5de5b60dbfc34422d5c0d2
Otávio Wolff Buffon <100782160+taviowolff@users.noreply.github.com>
2024-08-12 18:30:03 -0300

    Create Vd181.py

---
+++ Novo_curso/Aulas/Seção_4/Aula_82/Vd181.py
@@ -0,0 +1,11 @@
+# Ambientes virtuais em Python (venv)
+# Um ambiente virtual carrega toda a sua instalação
+# do Python para uma pasta no caminho escohido. 
+# Ao ativar o ambiente virutal, a instalação do 
+# ambiente virutal será usada.
+# venv é o módulo que vamos usar para 
+# criar ambientes virtuais.
+# Você pode dar o nome que preferir para um 
+# ambiente virtual, mas os mais comuns são:
+# venv env .venv .env
+
