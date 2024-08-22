3457b1a6f56c0793103fe825e568b98ed2c69539
Otávio Wolff Buffon <100782160+taviowolff@users.noreply.github.com>
2024-08-22 13:33:56 -0300

    2024/08/22

--- Novo_curso/Aulas/Seção_4/Aula_86/Vd189.py
+++ Novo_curso/Aulas/Seção_4/Aula_86/Vd189.py
@@ -12,4 +12,13 @@ with open(caminho_arquivo,'w+', encoding='UTF-8') as arquivo:
 
 sleep(3)
 
+# removendo
+# os.remove('C:\\Users\\otavi\\OneDrive\\Documentos\\GitHub\\Progress_Python\\Novo_curso\\Aulas\\Seção_4\\Aula_86\\aula86.txt') 
+# ou
 # os.remove(caminho_arquivo)
+
+# renomeando
+# os.rename(caminho_arquivo, 'arquivo86.txt')
+
+novo_nome_arquivo = os.path.join(os.path.dirname(caminho_arquivo), 'arquivo86.txt')
+os.rename(caminho_arquivo, novo_nome_arquivo)
