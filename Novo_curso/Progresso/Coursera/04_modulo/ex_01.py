8df7dde0968471922dc3b81bd1b59b8d4711e808
Otávio Wolff Buffon <100782160+taviowolff@users.noreply.github.com>
2024-09-11 16:49:01 -0300

    20240911

--- Novo_curso/Progresso/Coursera/04_modulo/ex_01.py
+++ Novo_curso/Progresso/Coursera/04_modulo/ex_01.py
@@ -1,12 +1,15 @@
-numlist = list()
+# numlist = list()
 
-while True:
-    inp = input('Digite um número: ')
-    if inp == 'done':
-        break
-    value = float(inp)
-    numlist.append(value)
+# while True:
+#     inp = input('Digite um número: ')
+#     if inp == 'done':
+#         break
+#     value = float(inp)
+#     numlist.append(value)
 
-media = sum(numlist) / len(numlist)
-print(media)
+# media = sum(numlist) / len(numlist)
+# print(media)
  
+friends = [ 'Joseph', 'Glenn', 'Sally' ]
+friends.sort()
+print(friends[0])
