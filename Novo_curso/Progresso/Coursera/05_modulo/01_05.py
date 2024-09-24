24a2c8a734aa142c50702ed0206281c6437fca2f
Otávio Wolff Buffon <100782160+taviowolff@users.noreply.github.com>
2024-09-24 15:16:43 -0300

    20240924

--- Novo_curso/Progresso/Coursera/05_modulo/01_05.py
+++ Novo_curso/Progresso/Coursera/05_modulo/01_05.py
@@ -21,8 +21,3 @@
 
 # stuff = dict()
 # print(stuff.get('candy',-1))
-
-if key in counts:
-    counts[key] = counts[key] + 1
-else:
-    counts[key] = 1
---
+++ Novo_curso/Progresso/Coursera/05_modulo/02_05.py
@@ -0,0 +1,39 @@
+# counts = dict()
+
+# print('escreva a linha de texto')
+# line = input('')
+
+# words = line.split()
+
+# print('Words:', words)
+
+# print('Counting...')
+
+# for word in words:
+#     counts[word] = counts.get(word,0) + 1
+# print('Counts', counts)
+
+jjj = {
+    'Chuck':1,
+    'Davince':42,
+    'Miquelagelo':100,
+}
+# print(list(jjj))
+
+# print(list(jjj.keys()))
+
+# print(list(jjj.values()))
+
+# print(list(jjj.items()))
+
+for aaa,bbb in jjj.items():
+    print(aaa,bbb)
+
+
+# O que é uma coleção
+# Listas versus dicionários
+# Constantes de dicionário
+# A palavra mais comum
+# Usando o método get()
+# Escrevendo loops de dicionário
+# Vislumbre: Tuplas
