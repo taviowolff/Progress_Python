46365763f63740fa7a61a399f8841c7833037bd5
Otávio Wolff Buffon <100782160+taviowolff@users.noreply.github.com>
2024-09-26 15:56:44 -0300

    20240926

---
+++ Novo_curso/Progresso/Coursera/06_modulo/01_06.py
@@ -0,0 +1,33 @@
+# x = ('Glenn', 'Sally', 'Joseph')
+# print(x[2]) # Retorno: Joseph
+
+# y = (1, 9, 2)
+# print(y) # (1, 9, 2)
+
+# print(max(y))
+
+# # iteravel
+
+# for iter in y:
+#     print(iter)
+
+
+# mas tuplas nao sao mutaveis 
+# y = 'abc'
+# y[2] = 'D' # TypeError: 'tuple' object does not support item assignment
+
+# z = (5,4,3)
+# z[2] = 0 # TypeError: 'tuple' object does not support item assignment
+
+
+# print(dir(tuple()))
+
+# Tuples and Assignment 
+
+# Tuples are comparable
+
+# Sorting lists of tuples
+
+# Using sorted()
+
+# Sort by Values instead of kry
