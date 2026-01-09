1c0b5ab5d5c06f0087c2ee5f0f32915537a1c900
otavio-wolff <otavio.buffon@hotmail.com>
2026-01-08 21:51:37 -0300

    Update funcoes_interfaces.py

--- revisao/ThinkPython/funcoes_interfaces.py
+++ revisao/ThinkPython/funcoes_interfaces.py
@@ -1,4 +1,3 @@
-@ -1,28 +1,10 @@
 import turtle
 from time import sleep
 
@@ -7,7 +6,7 @@ t = turtle.Turtle()
 
 # Agora usamos o 't' para dar comandos
 # t.forward(100)
-t.forward(59900)
+# t.forward(500)
 
 # t.left(90)
 
@@ -18,14 +17,29 @@ t.forward(59900)
 # t.forward(-100)
 
 # tocar linha inicial e final
-t.goto(0, 100)  # Move para o ponto (0, 100)
-sleep(2)
-t.goto(100, 100)  # Move para o ponto (0, 0)
-sleep(2)
-t.goto(100, 0)
-sleep(2)
-t.goto(0, 0)  # Volta para o ponto inicial (0, 0)
-sleep(2)
+# t.goto(0, 100)  # Move para o ponto (0, 100)
+# sleep(2)
+# t.goto(100, 100)  # Move para o ponto (0, 0)
+# sleep(2)
+# t.goto(100, 0)
+# sleep(2)
+# t.goto(0, 0)  # Volta para o ponto inicial (0, 0)
+# sleep(2)
+
+
+def square(direction = "left"):
+    for i in range(5):
+        t.forward(100)
+        if direction == "left":
+            t.left(72)
+        else:
+            t.right(72)
+        
+square("left")
+square("right") 
+
+
 # Importante: mantém a janela aberta
 turtle.done()
-turtle.done()
+
+# def triangle():
